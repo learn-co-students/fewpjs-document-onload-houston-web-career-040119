be8bd68123d9a794199c178ca18b45681cda580a
BagelEnthusiast <nathan.richardson94@gmail.com>
2019-05-14 17:29:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// Your code goes here\
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+   let stuff = document.getElementById("text")
+   stuff.innerHTML = "This is really cool!"
+   return stuff
+  });
