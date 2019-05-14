61061995ac4fddabcb874ec2ae2d3245a71983ee
ryanden2018 <denlinger.ryan@gmail.com>
2019-05-14 16:48:04 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded",function() {
+  let paragraph = document.querySelector("p#text");
+  paragraph.innerHTML = "This is really cool!";
+
+});
