52a6961cdf5990468a060f957ea499fef974f962
PYN <yxp7531540@gmail.com>
2019-05-14 20:56:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let paragrath = document.querySelector('#text')
+    paragrath.textContent = "This is really cool!"
+  });
