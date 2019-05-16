9728f0b0ee6605cac6b672d494f293d4c4abeb2b
Thomas_Hansen <ctrhansen@yahoo.com>
2019-05-16 12:57:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    updateDOM()
+    
+});
+
+function updateDOM() {
+  document.getElementById("text").innerHTML = "This is really cool!";
+  debugger
+}
+
+//when we add an event listener we need to update the DOM. Hence the function updateDOM()
